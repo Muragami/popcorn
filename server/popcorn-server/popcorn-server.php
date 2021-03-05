ace304dc6879ff160732f76b82d43be72bf2ddfd
JasonP <muragami@wishray.com>
2021-03-05 12:21:40 -0500

    alrighty then

--- server/popcorn-server/popcorn-server.php
+++ server/popcorn-server/popcorn-server.php
@@ -40,6 +40,101 @@ function pc_hello(WP_REST_Request $request) {
   // return information about this popcorn server!
   $ret['charset'] = DB_CHARSET;
   $ret['owner'] = $opts['server_owner'];
+  // setup the database for use!
+  $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+  if ($conn->connect_error) { return new WP_Error('pc_init() db connect', $conn->connect_error, array('status' => 400)); }
+  // are we initialized?
+  if ($conn->query("SELECT data FROM ps01_class WHERE id=1") === TRUE) {
+    $ret['initialized'] = true;
+  } else { $ret['initialized'] = false; }
+  return $ret;
+}
+
+// init the database structure
+function pc_init(WP_REST_Request $request) {
+  $ret = "";
+  // setup the database for use!
+  $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+  if ($conn->connect_error) { return new WP_Error('pc_init() db connect', $conn->connect_error, array('status' => 400)); }
+
+  $sql = "CREATE TABLE ps01_thing (
+  id BIGINT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  data JSON NOT NULL,
+  class BIGINT(8) UNSIGNED NOT NULL,
+  born TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )";
+
+  if ($conn->query($sql) === TRUE) { $ret .= "ok: ps01_thing table created; "; }
+    else { $ret .= "err: " . $conn->error . "; ";  }
+
+  $sql = "CREATE TABLE ps01_class (
+  id BIGINT(8) UNSIGNED PRIMARY KEY,
+  data JSON NOT NULL,
+  parent BIGINT(8) UNSIGNED
+  )";
+
+  if ($conn->query($sql) === TRUE) { $ret .= "ok: ps01_class table created; "; }
+    else { $ret .= "err: " . $conn->error . "; ";  }
+
+  $sql = "CREATE TABLE ps01_group (
+  id BIGINT(8) UNSIGNED PRIMARY KEY,
+  data JSON NOT NULL,
+  parent BIGINT(8) UNSIGNED
+  )";
+
+  if ($conn->query($sql) === TRUE) { $ret .= "ok: ps01_group table created; "; }
+    else { $ret .= "err: " . $conn->error . "; ";  }
+
+  $conn->close();
+  return $ret;
+}
+
+// boot with our default data, only if we have the proper code!
+function pc_boot(WP_REST_Request $request) {
+  $opts = get_option( 'popcorn_server_option_name' ); // get cfg options
+  $ret = "";
+  // setup the database for use!
+  $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+  if ($conn->connect_error) { return new WP_Error('pc_init() db connect', $conn->connect_error, array('status' => 400)); }
+  // make sure we are the owner
+  if ($request['code'] != $opts['unique_code']) {
+    return new WP_Error('pc_boot() auth', "bad code token", array('status' => 400)); }
+  // clear and reset tables
+  $sql = "TRUNCATE TABLE ps01_thing;TRUNCATE TABLE ps01_class;TRUNCATE TABLE ps01_group;";
+  if ($conn->multi_query($sql) === TRUE) { $ret .= "ok: all tables reset; "; }
+    else { $ret .= "err: " . $conn->error . "; ";  }
+  while($conn->next_result());
+  // insert minimal data needed
+  // first classes
+  $sql = "";
+  $i=0;
+  $cls = array();
+  $lines=explode("\n",file_get_contents(WP_PLUGIN_DIR."/popcorn-server/class.json"));
+  foreach($lines as $line) {
+    $line = trim($line);
+    if (substr($line, -1) == ',') { $line = substr($line,0,-1); }
+    if ($line != "[" && $line != "]" && $line != "") {
+      $sql .= "INSERT INTO ps01_thing (data,class) VALUES ('$line','0'); ";
+      $cls[$i] = $line;
+      $i += 1;
+    }
+  }
+  if ($conn->multi_query($sql) === TRUE) { $ret .= "ok: classes loaded as things; "; }
+    else { $ret .= "err: " . $conn->error . "; ";  }
+  while($conn->next_result());
+  $i = 0;
+  $sql = "";
+  while ($i < count($cls)) {
+    $id = $i + 1;
+    $line = $cls[$i];
+    $sql .= "INSERT INTO ps01_class (id,data) VALUES ($id,'$line'); ";
+    $i += 1;
+  }
+  if ($conn->multi_query($sql) === TRUE) { $ret .= "ok: classes registered as classes; "; }
+    else { $ret .= "err: " . $conn->error . "; ";  }
+  while($conn->next_result());
+  $conn->close();
+  //$ret .= $sql;
   return $ret;
 }
 
@@ -50,6 +145,16 @@ add_action( 'rest_api_init', function () {
   register_rest_route( 'popcorn-server/v1', '/hello', array(
     'methods' => 'GET',
     'callback' => 'pc_hello',
+    'permission_callback' => '__return_true',
+  ) );
+  register_rest_route( 'popcorn-server/v1', '/init', array(
+    'methods' => 'GET',
+    'callback' => 'pc_init',
+    'permission_callback' => '__return_true',
+  ) );
+  register_rest_route( 'popcorn-server/v1', '/boot', array(
+    'methods' => 'GET',
+    'callback' => 'pc_boot',
     'permission_callback' => '__return_true',
   ) );
 } );
