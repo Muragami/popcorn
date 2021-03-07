d7c2d292a9d7648dc758d06b6ad8e62b3044da95
JasonP <muragami@wishray.com>
2021-03-07 12:37:28 -0500

    exec.php does error checking

--- server/popcorn-server/exec.php
+++ server/popcorn-server/exec.php
@@ -1,6 +1,21 @@
 <?php
 require 'pop.php';
-echo $_GET["do"];
-$d = json_decode($_GET["do"],true);
-include('__' . $d['cmd'] . '.php');
+$ret = array();
+if (!isset($_GET['do'])) {
+  $ret[] = "err: missing do";
+} else {
+  $d = json_decode($_GET["do"],true);
+  if ($d == NULL) { $ret[] = "err: improper JSON in do"; echo json_encode($ret); }
+  if (!isset($d['cmd'])) {
+    $ret[] = "err: no cmd field provided";
+  } else {
+    $fn = '__' . $d['cmd'] . '.php';
+    if (!file_exists($fn)) {
+      $ret[] = "err: bad cmd: " . $d['cmd'];
+    } else {
+      include($fn);
+    }
+  }
+}
+echo json_encode($ret);
 ?>
